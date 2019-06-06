be8cbbd9b26a626966920d2166937e2837889ad6
Tejaswini <tejaswinivaichal@test.example.com>
2019-06-06 21:31:36 +0530

    newfiles

---
+++ even.py
@@ -0,0 +1,10 @@
+a = 100
+b = 0
+while a in range (100,200):
+    if (a %2 ==0):
+        b=b+a
+    a+=1
+
+print (b)
+
+        
---
+++ while.py
@@ -0,0 +1,18 @@
+list = ["a",11,"b","c","ff",25.5,55,77]
+c = 0
+a = []
+b = []
+d = []
+while ( c < len(list)):
+    if(type(list[c])==str):
+        a.append(list[c])
+    c+=1
+    elif(type(list[c])==int):
+        b.append(list[c])
+    c+=1
+    else :
+        d.append(list[c])
+    c+=1
+print (a)
+print (b)
+print (d)
