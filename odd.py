eb17899bad214810a5441a046da8bb6de949661e
Tejaswini <tejaswinivaichal@test.example.com>
2019-06-06 18:58:30 +0530

    even odd

---
+++ odd.py
@@ -0,0 +1,14 @@
+a = 100
+b = 0
+c = 0
+while a in range (100,200):
+    if (a %2 ==1):
+        b=b+a
+    else :
+        c=c+a
+    a+=1
+
+print ("odd number addition",b)
+print ("even number addition",c)
+
+        
